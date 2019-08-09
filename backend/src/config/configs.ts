faf14ec16ca983c449364c1a6596fd99c4c51dd4
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-08-09 16:20:13 -0300

    Code refactor in express server

---
+++ backend/src/config/configs.ts
@@ -0,0 +1,5 @@
+export default class Config {
+  public static port: number = 3000;
+  public static routes: string = "./dist/routes/**/*.js";
+  public static models: string = "./dist/models/**/*.js";
+}
--- backend/src/config/express.ts
+++ backend/src/config/express.ts
@@ -1,5 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import Config from "./configs";
+
 const app = express();
-app.listen(3000, () => {
-  // console.log("Running on 300")
+
+app.listen(Config.port, () => {
+  console.log("Running on 3000");
+});
+
+app.get("/", (res: Request, resp: Response ) => {
+  resp.send("Olá");
 });
