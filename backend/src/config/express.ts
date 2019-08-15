4b05c3bba9ec6e8d34c9f8466433b908fb344e39
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-08-15 11:30:27 -0300

    Creating connection with mongo

--- backend/src/config/express.ts
+++ backend/src/config/express.ts
@@ -1,5 +1,6 @@
 import bodyParser, { urlencoded } from "body-parser";
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import * as path from "path";
 import Config from "./configs";
 
@@ -12,6 +13,12 @@ for (const route of Config.globFiles(Config.routes)) {
   console.log(route);
   require(path.resolve(route)).default(app);
 }
+
+mongoose.connect("mongodb://localhost/Church_Project", (err) => {
+  if (err) {
+    console.log("Connection error");
+  }
+});
 
 app.listen(Config.port, () => {
   console.log("Running on 3000");
