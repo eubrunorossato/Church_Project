3f74c08e4c3564b6520d44c21d1907509fe7133f
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-08-16 18:34:22 -0300

    fixing

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Test from "./test";
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Test txt="teste"/>
+            </div>
+        )
+    }
+}
