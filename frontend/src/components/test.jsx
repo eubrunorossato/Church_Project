c11391f90eea62d006aaf3d32eab263ff54418c5
Bruno Rossato de Araujo <bruno.araujo@ibm.com>
2019-08-18 00:52:39 -0300

    inital react stack with bootstrap

--- frontend/src/components/test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default props => (
-    <div>
-        <h1>{props.txt}</h1>
-    </div>
-)
---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default props => (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button className="btn btn-danger">Bootstrap</button>
+      </header>
+    </div>
+)
