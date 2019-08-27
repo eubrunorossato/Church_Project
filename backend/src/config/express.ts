94a32163ebf87f67ec5a4a67ec96731b853b1c0a
Bruno Rossato <bruno.araujo@ibm.com>
2019-08-27 11:25:47 -0300

    Docker build fixed

--- backend/src/config/express.ts
+++ backend/src/config/express.ts
@@ -3,11 +3,11 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import * as path from "path";
 import Config from "./configs";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -15,10 +15,10 @@ for (const route of Config.globFiles(Config.routes)) {
   console.log(route);
   require(path.resolve(route)).default(app);
 }
-  mongoose.connect("mongodb://localhost:27017/church_project", (err) => {
-      console.log(err);
-    })
-    
+mongoose.connect(process.env.MONGODB_DSN || '"mongodb://db:27017/church_project"', (err) => {
+  console.log(err);
+})
+
 app.listen(Config.port, () => {
   console.log("Running on 3000");
 });
