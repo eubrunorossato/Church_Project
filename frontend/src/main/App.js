877db8a000babdddca47f11c830f840ae36d9d92
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-05 16:51:59 -0300

    member list route

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from "../components/navBar/navBar";
 import M from "materialize-css";
+import MemberList from "../components/memberList/memberList"
 import "materialize-css/dist/css/materialize.min.css";
 import 'material-design-icons/iconfont/material-icons.css';
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => (
-  <NavBar />
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/memberList" component={MemberList} />
+    </Switch>
+  </BrowserRouter>
 )
 
 
