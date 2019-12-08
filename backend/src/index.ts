c718648c043942b22bfb4fdd510964891b327863
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-08 12:58:37 -0300

    create express instance

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+import { ServerConfig } from "./config/config";
+import server from "./config/express";
+
+const configPort = ServerConfig.serverPort;
+
+server.listen(configPort, () => {
+  console.log(`Server running on port ${configPort}`);
+});
