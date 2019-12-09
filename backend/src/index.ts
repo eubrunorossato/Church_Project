fdd5d95d5ef4844aacb26d3ec720919646230c88
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-09 15:41:36 -0300

    mongoose set up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
+import express from "express";
 import { ServerConfig } from "./config/config";
-import server from "./config/express";
 
 const configPort = ServerConfig.serverPort;
+const server = express();
 
 server.listen(configPort, () => {
   console.log(`Server running on port ${configPort}`);
