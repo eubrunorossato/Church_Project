bc5e396919047d5283f06772d7d12ab0e51d3e32
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-10 14:50:17 -0300

    celula schema

---
+++ backend/src/dbConfigs/schemas/celula.schema.ts
@@ -0,0 +1,15 @@
+import { Model, model, Schema } from "mongoose";
+import { ICelula } from "../schemaInterfaces/celula.interface";
+
+const celulaSchema: Schema = new Schema({
+  celulaAddress: String,
+  celulaApt: Number,
+  celulaLeader: String,
+  celulaName: String,
+  celulaTime: Date,
+  celulaTrainingLeader: String,
+});
+
+const celula: Model<ICelula> = model<ICelula>("Celula", celulaSchema);
+
+export { celula };
