77472594da56dc0a880f4a43cf4a281e23f146b8
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-10 14:50:10 -0300

    celula interface

---
+++ backend/src/dbConfigs/schemaInterfaces/celula.interface.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export interface ICelula extends mongoose.Document {
+  celulaName: string;
+  celulaAddress: string;
+  celulaApt?: number;
+  celulaLeader: string;
+  celulaTime: Date;
+  celulaTrainingLeader?: string;
+}
