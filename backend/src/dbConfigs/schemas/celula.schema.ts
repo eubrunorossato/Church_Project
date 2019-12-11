7238891df2e60d261788cb7ab844e17626c4f322
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-11 17:16:38 -0300

    create a mongoose schema to celula

--- backend/src/dbConfigs/schemas/celula.schema.ts
+++ backend/src/dbConfigs/schemas/celula.schema.ts
@@ -4,6 +4,7 @@ import { ICelula } from "../schemaInterfaces/celula.interface";
 const celulaSchema: Schema = new Schema({
   celulaAddress: String,
   celulaApt: Number,
+  celulaIsOn: Boolean,
   celulaLeader: String,
   celulaName: String,
   celulaTime: Date,
