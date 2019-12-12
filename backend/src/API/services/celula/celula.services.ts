0cf37ea8ee8aa7157f5dc6a6e2895e4dd1ce1a71
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-12 16:19:59 -0300

    celula services

---
+++ backend/src/API/services/celula/celula.services.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { ICelula } from "../../../dbConfigs/schemaInterfaces/celula.interface";
+import celulaSchema from "../../../dbConfigs/schemas/celula.schema";
+import { IResponse } from "../responseInterfaces/findResponse.interface";
+
+const catchCelulas = async (req: Request, res: Response) => {
+  const celulaFilter: object = req.body.celulaName ? req.body : {};
+  const celulaList: ICelula[] = await celulaSchema.find(celulaFilter, (error: Error) => {
+    if (error) {
+      const response: IResponse = {
+        message: error.message,
+        status: false,
+      };
+      res.json({
+        response,
+      });
+    }
+  });
+
+  if (celulaList.length === 0) {
+    const response: IResponse = {
+      message: "Nenhuma célula foi encontrada",
+      status: false,
+    };
+    res.json({
+      response,
+    });
+  } else {
+    const response: IResponse = {
+      data: celulaList,
+      status: true,
+    };
+    res.json({
+      response,
+    });
+  }
+
+};
+
+const createNewCelula = async (req: Request, res: Response) => {
+  const body: ICelula = req.body;
+  const newCelulaJson: object = {
+    celulaAddress: body.celulaAddress,
+    celulaApt: body.celulaApt,
+    celulaIsOn: true,
+    celulaLeader: body.celulaLeader,
+    celulaName: body.celulaName,
+    celulaTime: body.celulaTime,
+    celulaTrainingLeader: body.celulaTrainingLeader,
+  };
+
+  await celulaSchema.create(newCelulaJson, (err: Error) => {
+    if (err) {
+      const response: IResponse = {
+        message: err.message,
+        status: false,
+      };
+      res.json({
+        response,
+      });
+    }
+  });
+
+  const response: IResponse = {
+    message: "Celula criada com sucesso",
+    status: true,
+  };
+  res.json({
+    response,
+  });
+
+};
+
+export {
+  catchCelulas,
+  createNewCelula,
+};
