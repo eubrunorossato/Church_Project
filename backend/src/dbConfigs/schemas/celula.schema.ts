a9aed3a896cdf5fcdfad7b5af901677078e5ae10
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-12 16:20:36 -0300

    celula schema

--- backend/src/dbConfigs/schemas/celula.schema.ts
+++ backend/src/dbConfigs/schemas/celula.schema.ts
@@ -2,13 +2,13 @@ import { Model, model, Schema } from "mongoose";
 import { ICelula } from "../schemaInterfaces/celula.interface";
 
 const celulaSchema: Schema = new Schema({
-  celulaAddress: String,
-  celulaApt: Number,
-  celulaIsOn: Boolean,
-  celulaLeader: String,
-  celulaName: String,
-  celulaTime: Date,
-  celulaTrainingLeader: String,
+  celulaAddress: { required: true, type: String },
+  celulaApt: { required: false, type: Number },
+  celulaIsOn: { required: true, type: Boolean },
+  celulaLeader: { required: true, type: String },
+  celulaName: { required: true, type: String },
+  celulaTime: { required: true, type: String },
+  celulaTrainingLeader: { required: false, type: String },
 });
 
 const celula: Model<ICelula> = model<ICelula>("Celula", celulaSchema);
