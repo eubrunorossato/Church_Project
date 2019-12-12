edb4bfc0e19468ffadc6cc43c0b0e2dd39deb188
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-12 16:47:26 -0300

    celulas route

---
+++ backend/src/API/routes/celula.routes.ts
@@ -0,0 +1,10 @@
+import { Express } from "express";
+import { catchCelulas, createNewCelula, updateCelulaState } from "../services/celula/celula.services";
+
+const CelulaRoutes = (app: Express) => {
+  app.route("/api/celula/get-all-celulas").get(catchCelulas);
+  app.route("/api/celula/create-celula").post(createNewCelula);
+  app.route("/api/celula/update-celula").put(updateCelulaState);
+};
+
+export { CelulaRoutes };
