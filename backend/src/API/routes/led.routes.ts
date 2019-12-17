745d22be8c8e6f308b25c2d826b95b2328d167a3
Bruno Araujo <bruno.araujo@ibm.com>
2019-12-16 23:44:29 -0300

    create led routes

--- backend/src/API/routes/led.routes.ts
+++ backend/src/API/routes/led.routes.ts
@@ -1,9 +1,10 @@
 import { Express } from "express";
-import { catchAllLeds, createLed } from "../services/led.services";
+import { catchAllLeds, createLed, updateLedFields } from "../services/led.services";
 
 const ledRoutes = (app: Express) => {
   app.route("/api/leader/get-all-leds").get(catchAllLeds);
   app.route("/api/leader/create-leds").post(createLed);
+  app.route("/api/leader/update-leds").put(updateLedFields);
 };
 
 export { ledRoutes };
